c144482307d54f7d56d64ce9e4244fc820f84b07
Conor C. Peterson <conorpetersondev@gmail.com>
2023-06-05 18:08:34 +0200

    feat: complete tutorial part 5

--- snippets/views.py
+++ snippets/views.py
@@ -5,8 +5,11 @@ from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 #from rest_framework import status
 #from rest_framework.decorators import APIView
-#from rest_framework.response import Response
+from rest_framework.response import Response
 #from rest_framework import mixins
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
 ## Function-based view example
@@ -81,6 +84,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -88,3 +99,10 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
